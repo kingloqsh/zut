69df26bfca4d1b28193fb72782dbba59ef49faa5
kingloqsh <69797707+kingloqsh@users.noreply.github.com>
2020-12-12 20:31:27 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+  
+window.addEventListener("scroll" ,function() {
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+$(document).ready(function(){
+  $('.header-burger').click(function(event){
+    $('.header-burger,.header-menu').toggleClass('active');
+    $(body).toggleClass('lock')
+  });
+});
+
